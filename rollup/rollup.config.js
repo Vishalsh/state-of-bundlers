1d3772fddfea26817fe49dbbf16b420187465bf3
Shaswat Prabhat <spd1238@gmail.com>
2022-10-28 18:18:58 +0530

    integrate rollup

---
+++ rollup/rollup.config.js
@@ -0,0 +1,42 @@
+import serve from "rollup-plugin-serve";
+import livereload from "rollup-plugin-livereload";
+import babel from "@rollup/plugin-babel";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import replace from "@rollup/plugin-replace";
+import image from "@rollup/plugin-image";
+import postcss from "rollup-plugin-postcss";
+
+export default {
+  input: "../app/index.js",
+  output: {
+    file: "dist/bundle.js",
+    format: "iife",
+    sourcemap: true,
+  },
+  plugins: [
+    babel({
+      presets: ["@babel/preset-react"],
+    }),
+    serve({
+      open: true,
+      verbose: true,
+      contentBase: ["", "dist"],
+      historyApiFallback: true,
+      host: "localhost",
+      port: 3000,
+    }),
+    livereload({ watch: "dist" }),
+    nodeResolve({
+      extensions: [".js", ".jsx"],
+    }),
+    postcss({
+      extensions: [".css"],
+    }),
+    image(),
+    commonjs(),
+    replace({
+      "process.env.NODE_ENV": JSON.stringify("development"),
+    }),
+  ],
+};
